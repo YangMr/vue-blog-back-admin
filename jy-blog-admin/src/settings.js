08f601c959b2fd486e40d4a59f5fbedeeecb93ff
yangmr <759168442@qq.com>
2020-11-10 13:54:24 +0800

    创建项目并进行初始化

---
+++ jy-blog-admin/src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+  //设置浏览器的标题
+  title: '积云博客后台权限管理系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true
+}
